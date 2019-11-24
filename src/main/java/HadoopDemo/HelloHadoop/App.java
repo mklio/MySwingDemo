a32fd8a648fad2cb541e472c8df6d663f0d6b6d3
mklio <360769968@qq.com>
2019-11-24 15:59:47 +0800

    test01

---
+++ src/main/java/HadoopDemo/HelloHadoop/App.java
@@ -0,0 +1,15 @@
+package HadoopDemo.HelloHadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public String sayHello() {
+		return "Hello";
+	}
+}
