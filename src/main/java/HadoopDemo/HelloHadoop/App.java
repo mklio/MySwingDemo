01a6a65167fcc6af9976c9e84e663ee05434e45c
mklio <360769968@qq.com>
2019-11-25 10:55:42 +0800

    20191125002

--- src/main/java/HadoopDemo/HelloHadoop/App.java
+++ src/main/java/HadoopDemo/HelloHadoop/App.java
@@ -1,11 +1,32 @@
 package HadoopDemo.HelloHadoop;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		App app = new App();
+		try {
+			app.createFile();
+		} catch (Exception e) {
+			System.out.println("Exception!!!");
+
+		}
 	}
 
-	public String sayHello() {
-		return "Hello";
+	public void createFile() throws Exception {
+		System.out.println("Hello createFile!");
+
+		Configuration configuration = new Configuration();
+		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop:9000"), configuration, "root");
+		FSDataOutputStream outputStream = null;
+		//outputStream = fs.create(new Path("/test/test01.txt"));
+		//outputStream.writeChars("123");
+		outputStream.close();
+		fs.close();
 	}
 }
--- src/test/java/HadoopDemo/HelloHadoop/AppTest.java
+++ src/test/java/HadoopDemo/HelloHadoop/AppTest.java
@@ -5,22 +5,29 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 //import static junit.framework.Assert.*;
 import java.net.URI;
 
 public class AppTest {
-	@Test
-	public void testDemo() {
-		System.out.println("Hello AppTest!");
+	@Before
+	public void testBefore() {
+		System.out.println("Hello testBefore!");
 	}
 	
-	//@Test
+	@Test
 	public void testmkdir() throws Exception {
 		System.out.println("Hello testmkdir!");
 		
 		Configuration configuration = new Configuration();
 		FileSystem fs = FileSystem.get(new URI("hdfs://hadoop:9000"), configuration, "root");
-		fs.mkdirs(new Path("/test0"));
+		//fs.mkdirs(new Path("/test1"));
 		fs.close();
+	}
+	
+	@After
+	public void testAfter() {
+		System.out.println("Hello testAfter!");
 	}
 }
